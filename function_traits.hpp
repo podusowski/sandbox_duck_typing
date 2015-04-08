0dec8d9f37a9904971ba2faec799a0d0fe63b532
Piotr Podusowski <podusowski@gmail.com>
2015-04-08 20:52:21 +0200

    function_traits for converting lambda to std::function

---
+++ function_traits.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <functional>
+
+template<class Signature>
+struct function_type : public function_type<decltype(&Signature::operator())>
+{
+};
+
+template<class Result, class Class, class... Args>
+struct function_type<Result(Class::*)(Args...) const>
+{
+    typedef std::function<Result(Args...)> type;
+};
+
+template<class Result, class... Args>
+struct function_type<Result(*)(Args...)>
+{
+    typedef std::function<Result(Args...)> type;
+};
+
+auto l = []{};
+
+static_assert(std::is_same<std::function<void()>,
+                           function_type<decltype(l)>::type
+                          >::value, "");
