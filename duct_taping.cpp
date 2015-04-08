6a4ec159afce8bd960dadfe1aa910b6686dff094
Piotr Podusowski <podusowski@gmail.com>
2015-04-08 18:10:48 +0200

    non-argument function

--- duct_taping.cpp
+++ duct_taping.cpp
@@ -1,3 +1,40 @@
+#include <string>
+#include <map>
+#include <iostream>
+#include <functional>
+
+
+struct function
+{
+    function & operator = (std::function<void()> f)
+    {
+        this->f = f;
+        return *this;
+    }
+
+    ~function()
+    {
+        f();
+    }
+
+private:
+    std::function<void()> f;
+};
+
+struct object
+{
+    function & operator / (const std::string & name)
+    {
+        return members[name];
+    }
+
+private:
+    std::map<std::string, function> members;
+};
+
 int main()
 {
+    object obj;
+    obj / "hello" = [] { std::cout << "hello"; };
+    obj / "hello";
 }
