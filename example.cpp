10ec861e8bd0f4d17bd07e4cecc39066f2657fb7
Piotr Podusowski <podusowski@gmail.com>
2015-04-09 14:49:52 +0200

    [] op in example

--- example.cpp
+++ example.cpp
@@ -7,33 +7,40 @@
 
 #include "duck_typing/object.hpp"
 
-int main()
+object make_object()
 {
     object obj;
 
-    obj / "hello" = []
+    obj["hello"] = []
     {
         std::cout << "hello" << std::endl;
     };
 
-    obj / "hello1" = [] (int i)
+    obj["hello1"] = [] (int i)
     {
         std::cout << "hello " << i << std::endl;
     };
 
-    obj / "hello_with_string" = [] (std::string s)
+    obj["hello_with_string"] = [] (std::string s)
     {
         std::cout << "hello " << s << std::endl;
     };
 
-    obj / "hello_with_int_and_string" = [] (int i, std::string s)
+    obj["hello_with_int_and_string"] = [] (int i, std::string s)
     {
         std::cout << "hello " << i << " " << s << std::endl;
     };
 
+    return obj;
+}
+
+int main()
+{
+    object obj = make_object();
+
     obj["hello"]();
     obj["hello1"](1);
-    obj["hello_with_string"](std::string("Piotr"));
+    obj["hello_with_string"](std::string("Piotr")); // implicit conv doesn't work :(
     obj["hello_with_int_and_string"](2, std::string("Piotr"));
 
     try
