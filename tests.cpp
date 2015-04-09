4ff3ff3e69e0140e622f43b249e4153cf33f2913
Piotr Podusowski <podusowski@gmail.com>
2015-04-09 14:13:29 +0200

    some tests

--- tests.cpp
+++ tests.cpp
@@ -1,3 +1,42 @@
 #include <gtest/gtest.h>
 
+#include "duck_typing/object.hpp"
 
+TEST(object_tests, lambda_without_arguemnt_can_be_called)
+{
+    bool called = false;
+
+    object obj;
+
+    obj / "method" = [&called] ()
+    {
+        called = true;
+    };
+
+    EXPECT_FALSE(called);
+
+    (obj / "method")();
+
+    EXPECT_TRUE(called);
+}
+
+TEST(object_tests, lambda_with_one_arguemnt_can_be_called)
+{
+    bool called = false;
+    int arg1 = 1;
+
+    object obj;
+
+    obj / "method" = [&] (int i)
+    {
+        called = true;
+        arg1 = i;
+    };
+
+    EXPECT_FALSE(called);
+
+    (obj / "method")(2);
+
+    EXPECT_TRUE(called);
+    EXPECT_EQ(2, arg1);
+}
